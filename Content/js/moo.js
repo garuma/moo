fc59022a6c2ee00646d070d0ffb328b2f3a52930
Jérémie Laval <jeremie.laval@gmail.com>
2011-07-03 17:41:52 +0200

    Add permacow

--- Content/js/moo.js
+++ Content/js/moo.js
@@ -4,9 +4,14 @@ setTimeout (function () {
 		var input = $('#input');
 		var cow = $('#cow');
 		var type = $('#type');
+		var permacow = $('#permacow');
 
 		var process = function () {
-			$.post ('/moo', { message: input.val(), cowfile: type.val () }, function (data) {
+			var postdata = { message: input.val(), cowfile: type.val () };
+
+			permacow.attr('href', '/?permacow=' + escape(btoa(JSON.stringify(postdata))));
+
+			$.post ('/moo', postdata, function (data) {
 				cow.text (data);
 			}, 'text');
 		};
@@ -21,6 +26,22 @@ setTimeout (function () {
 		type.change(function (event) {
 			process ();
 		});
+
+		if (window.location.search.length > 1) {
+			var is, as = window.location.search.substr(1).split("&");
+			for (var i = 0; i < as.length; i++) {
+				is = as[i].split("=");
+				if (is.length != 2 || is[0] != 'permacow')
+					continue;
+
+				try {
+					var parsed = JSON.parse(atob(unescape(is[1])));
+					input.val(parsed.message);
+					type.val(parsed.cowfile);
+					process ();
+				} catch (e) { }
+			}
+		}
 	})(window.jQuery);
 }, 2);
 
